a752ede501b6f4aca0e7365a3d434d88fb711130
Tarif-dev <tarifhussain212@gmail.com>
2025-08-11 18:36:08 +0530

    tabs route configured

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,55 @@
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Tabs } from "expo-router";
+import React from "react";
+import { Image, ImageBackground, Text } from "react-native";
+
+const TabIcon = () => {
+  return (
+    <ImageBackground
+      source={images.highlight}
+      className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+    >
+      <Image source={icons.home} />
+      <Text className="text-secondary text-base font-semibold ml-2">Home</Text>
+    </ImageBackground>
+  );
+};
+
+const _Layout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (<TabIcon/>),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "Saved",
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          headerShown: false,
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default _Layout;
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,9 @@
 import { Text, View } from "react-native";
-import "./globals.css"
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-dark-200 font-bold">Welcome !</Text>
+      <Text className="text-5xl text-gray-800 font-bold">Welcome!</Text>
     </View>
   );
 }
