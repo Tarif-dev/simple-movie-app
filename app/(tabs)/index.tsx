0e0a2dbc5cb1c42467d2a9075aeb32f632adb464
Tarif-dev <tarifhussain212@gmail.com>
2025-08-15 23:18:38 +0530

    fetching hook created

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,28 +1,56 @@
-import { Text, View, Image ,ScrollView } from "react-native";
-import { images } from "@/constants/images";
+import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
-import SearchBar
- from "@/components/SearchBar";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
+import { ActivityIndicator, Image, ScrollView, View , Text } from "react-native";
 
 export default function Index() {
   const router = useRouter();
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: "" }));
+
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0"></Image>
 
-      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{
-        minHeight : "100%",
-        paddingBottom : 10
-      }}>
-          <Image source={icons.logo} className=" w-12 h-10 mt-20 mb-5 mx-auto"></Image>
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          minHeight: "100%",
+          paddingBottom: 10,
+        }}
+      >
+        <Image
+          source={icons.logo}
+          className=" w-12 h-10 mt-20 mb-5 mx-auto"
+        ></Image>
 
-          <View className="flex-1 mt-5">
-            <SearchBar
-              placeholder="Search for a movie"
-              onPress = {()=>router.push("/search")}
-            />
-          </View>
+        {moviesLoading ?(
+          <ActivityIndicator
+            size="large"
+            color = "#0000ff"
+            className="mt-10 self-center"
+          />
+        ): moviesError ? (
+          <Text>Error: {moviesError?.message}</Text>
+        ) : (
+        <View className="flex-1 mt-5">
+          <SearchBar
+            placeholder="Search for a movie"
+            onPress={() => router.push("/search")}
+          />
+          <>
+            <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
+          </>
+        </View>
+        )}
+
       </ScrollView>
     </View>
   );
